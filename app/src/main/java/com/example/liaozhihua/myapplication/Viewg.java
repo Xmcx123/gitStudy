73e7ca364051ce3cb8cb0d5fdee3dfaf2ff307dc
廖智华 <liaozhihua@liaozhihuadeMac-mini.local>
2019-05-14 10:15:36 +0800

    first commit

---
+++ app/src/main/java/com/example/liaozhihua/myapplication/Viewg.java
@@ -0,0 +1,15 @@
+package com.example.liaozhihua.myapplication;
+
+import android.content.Context;
+import android.widget.RelativeLayout;
+
+public class Viewg extends RelativeLayout {
+
+    public Viewg(Context context) {
+        super(context);
+
+  }
+
+
+
+}
